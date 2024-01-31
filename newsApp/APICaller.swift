9785fe485cc7bc801ee6956384482a95ccc4c679
Nikita Kislyakov <nikitakislyakovv@gmail.com>
2024-01-31 18:12:33 +0300

    newsApp without searching

---
+++ newsApp/APICaller.swift
@@ -0,0 +1,59 @@
+//
+//  APICaller.swift
+//  newsApp
+//
+//  Created by Никита Кисляков on 31.01.2024.
+//
+
+import Foundation
+
+class APICaller {
+    
+    static let shared = APICaller()
+    
+    struct Constants {
+        static let topHeadlinesURL = URL(string: "https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=584c633dc1a6417aaf0db906031a3a98")
+    }
+    
+    private init() {}
+    
+    public func getTopStories(completion: @escaping (Result<[Article], Error>) -> Void) {
+        guard let url = Constants.topHeadlinesURL else { return }
+        
+        let task = URLSession.shared.dataTask(with: url) { data, _, error in
+            if let error = error {
+                completion(.failure(error))
+            } 
+            else if let data = data {
+                
+                do {
+                    let result = try JSONDecoder().decode(APIResponse.self, from: data)
+                    completion(.success(result.articles))
+                }
+                catch {
+                    completion(.failure(error))
+                }
+            }
+        }
+        task.resume()
+    }
+}
+
+// Models
+
+struct APIResponse: Codable {
+    let articles: [Article]
+}
+
+struct Article: Codable {
+    let source: Source
+    let title: String
+    let description: String?
+    let url: String?
+    let urlToImage: String?
+    let publishedAt: String?
+}
+
+struct Source: Codable {
+    let name: String
+}
